301c150deb4ddc8ad84a9d3ddcc0fe282eb4d91a
him2016 <39089904+him2016@users.noreply.github.com>
2025-10-10 23:11:03 +0530

    Create MaximumProfit.java

---
+++ MaximumProfit.java
@@ -0,0 +1,27 @@
+public class MaximumProfit {
+
+    public static void main(String[] args) {
+        int[] startTime = {0, 4, 5};
+        int[] endTime = {3, 5, 7};
+        int[] profit = {1, 2, 2};
+
+        System.out.println(jobScheduling(startTime, endTime, profit));
+    }
+
+    private static int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int n = startTime.length;
+        int[][] jobs = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            jobs[i] = new int[]{startTime[i], endTime[i], profit[i]};
+        }
+        Arrays.sort(jobs, (a, b) -> a[1] - b[1]);
+        TreeMap<Integer, Integer> dp = new TreeMap<>();
+        dp.put(0, 0);
+        for (int[] job : jobs) {
+            int cur = dp.floorEntry(job[0]).getValue() + job[2];
+            if (cur > dp.lastEntry().getValue())
+                dp.put(job[1], cur);
+        }
+        return dp.lastEntry().getValue();
+    }
+}
