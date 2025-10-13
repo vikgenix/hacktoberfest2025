06690e3f4e7820dc2b1c6cd0d45cef036ac4ed24
nidhirathnakar <nidhirathnakar@gmail.com>
2025-10-14 00:33:20 +0530

    Create hashing-numbers.cpp
    This pull request adds a solution for the Hashing Numbers problem, which efficiently processes arrays or sequences of numbers using a hashing technique.

---
+++ hashing-numbers.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+using namespace std
+int main()
+{
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  int hash[n];
+  for(int i=0;i<n;i++){
+    hash[arr[i]]+=1;
+  }
+  int q;  //number of input values
+  cin>>q;
+  while(q--){
+    int number;
+    cin>>number;
+    cout<<hash[number]<<endl;
+  }
+  return 0;
+}
+
+  
